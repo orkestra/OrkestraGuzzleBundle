69e79e8d73f5ca73f0c0ca1ac58b309b3e4b73ec
Zach Badgett <zach.badgett@gmail.com>
2012-07-13 11:58:47 -0600

    Added the following attributes to Param annotation: default, doc, min_length, max_length, static, prepend, append, and filters; added extra attributes to JsonGeneratorDumper

--- Generator/Dumper/JsonGeneratorDumper.php
+++ Generator/Dumper/JsonGeneratorDumper.php
@@ -40,6 +40,35 @@ class JsonGeneratorDumper
                         $paramName = $param->getName();
                         $paramArray['type'] = $param->getType();
                         $paramArray['required'] = $param->getRequired();
+
+                        if (null !== $param->getLocation()) {
+                            $paramArray['location'] = $param->getLocation();
+                        }
+                        if (null !== $param->getDefault()) {
+                            $paramArray['default'] = $param->getDefault();
+                        }
+                        if (null !== $param->getDoc()) {
+                            $paramArray['doc'] = $param->getDoc();
+                        }
+                        if (null !== $param->getMinLength()) {
+                            $paramArray['min_length'] = $param->getMinLength();
+                        }
+                        if (null !== $param->getMaxLength()) {
+                            $paramArray['max_length'] = $param->getMaxLength();
+                        }
+                        if (null !== $param->getStatic()) {
+                            $paramArray['static'] = $param->getStatic();
+                        }
+                        if (null !== $param->getPrepend()) {
+                            $paramArray['prepend'] = $param->getPrepend();
+                        }
+                        if (null !== $param->getAppend()) {
+                            $paramArray['append'] = $param->getAppend();
+                        }
+                        if (null !== $param->getFilters()) {
+                            $paramArray['filters'] = $param->getFilters();
+                        }
+
                         $dumpArray['commands'][$name]['params'][$paramName] = $paramArray;
                     }
                 }
